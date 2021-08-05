fa3f3302c10083ed2ec8c58c91148ad97daf7705
KAMINWAR SAI PRIYA <k.saipriya08@gmail.com>
2021-08-05 13:27:27 +0530

    first commit

---
+++ src/main/java/com/mailorderpharma/drugservice/DrugServiceApplication.java
@@ -0,0 +1,16 @@
+package com.mailorderpharma.drugservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableFeignClients
+public class DrugServiceApplication extends SpringBootServletInitializer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrugServiceApplication.class, args);
+	}
+
+}
